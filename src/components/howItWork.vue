12c40b31f3ca7e0efc71a7f924165495c761d35e
E1-XP <poczta.elektroniczna@hotmail.com>
2023-02-17 13:27:49 +0100

    begin with how-it-work section markup and styles

---
+++ src/components/howItWork.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="how-it-work">
+    <header class="how-it-work__header">
+      <h2 class="heading">How It Work</h2>
+      <p class="subtitle">
+        A Simple, Proven Way to Boost Your Team Performance.
+      </p>
+      <p class="subtitle-2">
+        Most popular type of partnership Malta. The limited liability is, in
+        fact, the only type of company allowed by Companies.
+      </p>
+    </header>
+    <div class="how-it-work__actions"></div>
+    <figure class="how-it-work__picture"></figure>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.how-it-work {
+  &__header {
+    margin: 0 auto;
+  }
+}
+</style>
