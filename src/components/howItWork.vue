3fd65ce05a92cee5651ff0d180b1e83945c0be69
E1-XP <poczta.elektroniczna@hotmail.com>
2023-02-18 10:00:48 +0100

    adjust typography, finish header, add section image

--- src/components/howItWork.vue
+++ src/components/howItWork.vue
@@ -2,26 +2,65 @@
 
 <template>
   <section class="how-it-work">
-    <header class="how-it-work__header">
-      <h2 class="heading h-text-center">How It Work</h2>
-      <p class="subtitle h-text-center">
-        A Simple, Proven Way to Boost Your Team Performance.
-      </p>
-      <p class="subtitle-2">
-        Most popular type of partnership Malta. The limited liability is, in
-        fact, the only type of company allowed by Companies.
-      </p>
-    </header>
-    <div class="how-it-work__actions"></div>
-    <figure class="how-it-work__picture"></figure>
+    <div class="l-container how-it-work__container">
+      <header class="how-it-work__header">
+        <h2 class="heading h-text-center">How It Work</h2>
+        <p class="subtitle h-text-center how-it-work__subtitle">
+          A Simple, Proven Way to Boost Your Team Performance.
+        </p>
+        <p class="subtitle-2">
+          Most popular type of partnership Malta. The limited liability is, in
+          fact, the only type of company allowed by Companies.
+        </p>
+      </header>
+      <div class="how-it-work__actions"></div>
+      <figure class="how-it-work__picture">
+        <img
+          src="./../assets/images/Image.png"
+          alt="mobile app mockup"
+          class="how-it-work__image"
+          draggable="false"
+        />
+      </figure>
+    </div>
   </section>
 </template>
 
 <style scoped lang="scss">
 .how-it-work {
+  & :global(.l-container) {
+    display: grid;
+    grid-template-rows: auto auto;
+    grid-template-columns: 1fr 1fr;
+  }
+
   &__header {
+    grid-column: 1/3;
     margin: 0 auto;
-    width: 18.979rem;
+    margin-top: 2.667rem;
+    margin-bottom: 1.821rem;
+    width: 22.23rem;
+  }
+
+  &__subtitle {
+    width: 85%;
+    margin: 0 auto;
+    margin-top: 1.1rem;
+    margin-bottom: 0.91rem;
+  }
+
+  &__actions {
+    background-color: red;
+    height: 300px;
+  }
+
+  &__picture {
+    user-select: none;
+  }
+
+  &__image {
+    pointer-events: none;
+    -webkit-user-drag: none;
   }
 }
 </style>
