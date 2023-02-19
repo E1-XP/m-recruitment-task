135d267b6687f44c509c432a183101e36a3ec286
E1-XP <poczta.elektroniczna@hotmail.com>
2023-02-19 11:14:54 +0100

    finish statistics section, add shapes

--- src/components/statistics.vue
+++ src/components/statistics.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import FeatureCard from "./featureCard.vue";
 
+import Shape1 from "./backgrounds/shape1.vue";
+import Shape2 from "./backgrounds/shape2.vue";
+
 const data = [
   { title: "25K+", subtitle: "App Downloads" },
   {
@@ -25,6 +28,8 @@ const data = [
         </li>
       </ul>
     </div>
+    <Shape1 class="statistics__shape1" />
+    <Shape2 class="statistics__shape2" />
   </section>
 </template>
 
@@ -32,10 +37,44 @@ const data = [
 .statistics {
   background-color: $color-primary;
   color: $color-white;
+  padding: 8.76rem 0;
+  clip-path: polygon(0 0, 100% 10%, 100% 100%, 0 90%);
+  position: relative;
+
+  &__container {
+    display: flex;
+  }
 
   &__list {
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    width: 68%;
+    margin: 0 auto;
+  }
+
+  &__shape1 {
+    position: absolute;
+    bottom: 26%;
+    transform: scale(221%) translateX(28%) translateY(-16%);
+
+    @include bp($bp-very-large) {
+      transform: scale(169%) translateX(22%);
+    }
+
+    @include bp($bp-medium) {
+      transform: initial;
+    }
+  }
+
+  &__shape2 {
+    position: absolute;
+    bottom: 6%;
+    right: 0;
+    transform: scale(140%) translateX(-15%) translateY(-13%);
+
+    @include bp($bp-large) {
+      transform: initial;
+    }
   }
 }
 </style>
