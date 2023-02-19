ceed69cfafa5e671f17f6195ffe0bdc2c3b8200b
E1-XP <poczta.elektroniczna@hotmail.com>
2023-02-19 11:42:19 +0100

    move header into new file, reuse it

---
+++ src/components/header.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+export interface HeaderData {
+  heading: string;
+  subtitle: string;
+  subtitle2: string;
+}
+
+defineProps<{ data: HeaderData }>();
+</script>
+
+<template>
+  <header class="header">
+    <h2 class="heading h-text-center">{{ data.heading }}</h2>
+    <p class="subtitle h-text-center header__subtitle">
+      {{ data.subtitle }}
+    </p>
+    <p class="subtitle-2">
+      {{ data.subtitle2 }}
+    </p>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  width: 22.23rem;
+
+  &__subtitle {
+    width: 85%;
+    margin: 0 auto;
+    margin-top: 1.1rem;
+    margin-bottom: 0.91rem;
+  }
+}
+</style>
--- src/components/screenshots.vue
+++ src/components/screenshots.vue
@@ -1,10 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "./header.vue";
+
+const headerData = {
+  heading: "Screenshots",
+  subtitle: "Awesome Apps ScreenShots",
+  subtitle2:
+    "Most popular type of partnership Malta. The limited liability is, in fact, the only type of company allowed by Companies.",
+};
+</script>
 
 <template>
-  <div>ok</div>
+  <section class="screenshots">
+    <Header class="screenshots__header" :data="headerData" />
+  </section>
 </template>
 
 <style scoped lang="scss">
 .screenshots {
+  &__header {
+    margin: 0 auto;
+    margin-top: 2.667rem;
+    margin-bottom: 1.821rem;
+  }
 }
 </style>
