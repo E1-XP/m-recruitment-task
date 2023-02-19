97e2040f1136393234722f75fca64e9216a3194c
E1-XP <poczta.elektroniczna@hotmail.com>
2023-02-19 11:22:07 +0100

    add main tag into app, begin with screenshots section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import HowItWorkVue from "./components/howItWork.vue";
 import Statistics from "./components/statistics.vue";
+import Screenshots from "./components/screenshots.vue";
 </script>
 
 <template>
-  <HowItWorkVue />
-  <Statistics />
+  <main>
+    <HowItWorkVue />
+    <Statistics />
+    <Screenshots />
+  </main>
 </template>
 
 <style scoped lang="scss"></style>
